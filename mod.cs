f9ecd82d9cd23e7198a2b8e3f234271acf152f53
ContentApple <wesleyjensen05@gmail.com>
2026-03-01 15:33:44 -0600

    Warning
    Any attempt to reuse, redistribute, sell, or monetize this content without explicit permission from the creator will result in a DMCA takedown on Steam.

---
+++ mod.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mod
+{
+    public class Mod : MonoBehaviour
+    {
+        public static void Main()
+        {
+            ModAPI.Register<HoverInspectSticky>();
+        }
+    }
+
+    public class HoverInspectSticky : MonoBehaviour
+    {
+        private PersonBehaviour lastPerson;
+
+        private void Update()
+        {
+            Vector2 mouse = Global.main.MousePosition;
+            Collider2D hit = Physics2D.OverlapPoint(mouse);
+
+            PersonBehaviour p = null;
+
+            if (hit != null)
+            {
+                LimbBehaviour limb = hit.GetComponentInParent<LimbBehaviour>();
+                if (limb != null)
+                {
+                    p = limb.Person;
+                }
+            }
+
+            var inspector = Object.FindObjectOfType<LimbStatusViewBehaviour>(true);
+            if (inspector == null) return;
+
+            if (p == null)
+                return;
+
+            List<LimbBehaviour> validLimbs = new List<LimbBehaviour>();
+            foreach (var limb in p.Limbs)
+            {
+                if (limb != null)
+                    validLimbs.Add(limb);
+            }
+
+            if (validLimbs.Count == 0)
+                return;
+
+            if (p != lastPerson)
+            {
+                inspector.gameObject.SetActive(true);
+                inspector.Limbs = validLimbs;
+                lastPerson = p;
+            }
+        }
+    }
+}
